016a64e03b8711184dc27cb1df86672e336a9ac9
bennymi <xbenmmusicx@gmail.com>
2023-12-22 18:15:59 +0100

    remove comments

--- src/lib/components/toast/toaster.svelte
+++ src/lib/components/toast/toaster.svelte
@@ -33,35 +33,5 @@
 >
 	{#each $toasts as toast (toast.id)}
 		<Toast {elements} {toast} />
-		<!-- <div
-			{...$content(id)}
-			use:content
-			animate:flip={{ duration: 500 }}
-			in:fly={{ duration: 150, x: '100%' }}
-			out:fly={{ duration: 150, x: '100%' }}
-			class="rounded-lg bg-neutral-800 text-white shadow-md"
-		>
-			<div
-				class="relative flex w-[24rem] max-w-[calc(100vw-2rem)] items-center justify-between gap-4 p-5"
-			>
-				<div>
-					<h3 {...$title(id)} use:title class="flex items-center gap-2 font-semibold">
-						{data.type}
-						<span class="rounded-full w-4 h-4 {data.color}" />
-					</h3>
-					<div {...$description(id)} use:description>
-						{data.description}
-					</div>
-				</div>
-
-				<button
-					{...$close(id)}
-					use:close
-					class="absolute right-4 top-4 grid place-items-center rounded-full text-white hover:bg-primary/70"
-				>
-					<span class="text-xl i-material-symbols-close-rounded" />
-				</button>
-			</div>
-		</div> -->
 	{/each}
 </div>
